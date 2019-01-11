13b48b5bf4cefab20acee728537d048cfa4fb62c
hkKevin <pakkiuleung@gmail.com>
2019-01-11 11:44:19 +1100

    Added header

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import './App.css';
 import Memos from './containers/Memos';
+import Header from './components/Header/Header';
 
 class App extends Component {
   
@@ -9,6 +10,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <Memos />
       </div>
     );
---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './Header.css'
+
+const Header = () => (
+  <div>
+    <header className='header'>MEMO</header>
+  </div>
+);
+
+export default Header;
