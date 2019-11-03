20c1b58067f37e875ae1a517b9d2ba5f05fb86a9
hkKevin <pakkiuleung@gmail.com>
2019-11-03 15:22:24 +1100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/memo/precache-manifest.46b8870f3fc88c35e3e7d641d6b96ee2.js"
+  "/memo/precache-manifest.356919d31304c8514379f20f70000698.js"
 );
 
 workbox.clientsClaim();
